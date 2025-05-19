29ca8ecbb34e87b60fbd8c43a8d630761a93c787
simog <192517110+SimoG3@users.noreply.github.com>
2025-05-19 21:13:03 +0100

    hi this is my daily commit :)

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("abibas>>crocodilo niko NONONONONO")
+console.log("abibas>>crocodilo niko NONONONONO");
+
+document.getElementById("buttonWAW").addEventListener("click", () => {
+    document.getElementById("IMHIDDENHIHI").classList.remove("hidden");
+})
